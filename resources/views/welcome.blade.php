468644ccb966503d2593d833ed9ad1e48ba59378
ashwin <ashwin@usistech.com>
2019-09-03 05:32:41 +0530

    CRUD , USER login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,46 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href=" {{ asset('css/bootstrap.min.css') }}">
+</head>
+<body>
+    <p class="jumbotron">Welcome</p>
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <form id="loginform" name="loginform" action="{{ url('/login') }}" method="post">
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label class="form-label">Email </label>
+                                <input type="email" name="email" placeholder="Enter the Email" class="form-control" />
+                            @error('email')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label">Password</label>
+                                <input type="password" name="password" placeholder="Enter the Password" class="form-control" />
+                            @error('password')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        @csrf
+                        <input type="reset" class="btn btn-danger" name="reset" value="Reset" />
+                        <input type="submit" class="btn btn-primary" name="Submit" value="Submit" />
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+            </form>
+            @include('template.flash')
         </div>
-    </body>
+        <div class="col-lg-4"></div>
+    </div>
+</body>
+    <script type="text/javascript" src=" {{ asset('js/jquery.min.js') }}"></script>
+    <script type="text/javascript" src=" {{ asset('js/jquery.validate.min.js') }}"></script>
+    <script type="text/javascript" src=" {{ asset('js/bootstrap.min.js') }}"></script>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login',function(){
+	return view('welcome');
+});
+Route::post('/login','UserController@login');
+
+Route::get('/signin',function(){
+	return view('auth.signin');
+});
+Route::post('/signin','UserController@signin');
+Route::get('/logout','UserController@logout');
+
+
+// verifyuserloggedin
+Route::get('/products','UserController@index');
+Route::get('/product','UserController@create');
+Route::post('/product','UserController@store');
+Route::get('/product/{id}','UserController@edit')->where('id','[1-9]+');
+Route::post('/product/{id}','UserController@update')->where('id','[1-9]+');
+Route::delete('/product/{id}','UserController@destroy')->where('id','[1-9]+');
