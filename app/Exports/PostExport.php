17d91bef7506b67057383f659ff6de42d90b6e2d
ashwin <ashwin@usistech.com>
2019-10-28 08:34:00 +0530

    Demo -- Export Report -> One Element Changes ->

--- app/Exports/PostExport.php
+++ app/Exports/PostExport.php
@@ -8,9 +8,16 @@ use Maatwebsite\Excel\Concerns\FromView;
 
 class PostExport implements FromView
 {
+
+	public function __construct(){
+
+	}
+
     public function view() : View
     {
-    	$posts = Post::all();
-    	return view('exports.post.list', compact("posts"));
+    	// $posts = Post::where(["id" => 1])->get();
+    	// $posts = Post::all();
+    	$users = \App\Models\User::first();
+    	return view('exports.post.list', compact("users"));
     }
 }
--- resources/views/exports/post/list.blade.php
+++ resources/views/exports/post/list.blade.php
@@ -1,9 +1,18 @@
 <table>
+	<thead>
+		<tr>
+			<td>User</td>
+			<td style="color: red; background-color: black;">Comments</td>
+		</tr>
+	</thead>
 	<tbody>
-		@foreach($posts as $post) 
-			<tr>
-				<td>{{ $post->comments }}</td>
-			</tr>
+		@foreach($users as $user) 
+		<tr>
+			<td>{{ $user->name }}</td>
+		</tr>
 		@endforeach
+		@foreach($users->posts as $comment) 
+			<td>{{ $comment->comments ?? '' }}</td>
+		@endforeach		
 	</tbody>
 </table>
